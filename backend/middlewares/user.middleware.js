69d7a504e344b479d7ab567caed4c2988deff1b4
puja335 <shahipuja335@gmail.com>
2025-01-13 10:18:38 +0545

    changes user.middleware.js

--- backend/middlewares/user.middleware.js
+++ backend/middlewares/user.middleware.js
@@ -1,20 +1,21 @@
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
 function authenticateUser(req, res, next) {
-  const token = req.cookies.token;
+  console.log(req.headers.authorization);
+  const bearerToken = req.headers.authorization;
+
+  const token = req.cookies.token || bearerToken.split(" ")[1];
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    console.log(user, "User");
     // console.log(err,'Error');
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(403).json({ error: "Unauthorized", err });
     req.user = user;
     next();
   });
 }
 
-
-
-
-export default authenticateUser;;
+export default authenticateUser;
