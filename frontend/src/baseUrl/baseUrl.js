5931c80e4bfd88f03c08b91a584e27d823618c49
puja335 <shahipuja335@gmail.com>
2025-01-28 10:23:07 +0545

    modified base url for https

--- frontend/src/baseUrl/baseUrl.js
+++ frontend/src/baseUrl/baseUrl.js
@@ -1,4 +1,4 @@
 // export const baseUrl = 'https://movieticketmainbe-prod.up.railway.app';
 // export const baseUrl = 'https://filmgo-api.vercel.app';
 // export const baseUrl = 'https://movie-ticket-main-be.onrender.com';
-export const baseUrl = "http://localhost:3000"
+export const baseUrl = "https://localhost:5000"
