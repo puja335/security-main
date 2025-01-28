66493bd4577763ab1c9d5b6f3e77a7a9dfe0093e
puja335 <shahipuja335@gmail.com>
2025-01-28 10:20:15 +0545

    modified user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,32 +1,50 @@
-import express from 'express';
-import {Signup,Signin, Logout, checkUser, getUser} from '../controllers/user.controller.js';
-import { GetShowsByDate, ShowSeats } from '../controllers/show.controller.js';
-import { Movies,MovieDetails } from '../controllers/movie.controller.js';
-import authenticateUser from '../middlewares/user.middleware.js';
-import { createOrder, verifyPayment, viewBookingbyUser } from '../controllers/booking.controller.js';
-import checkSeatStatus from '../middlewares/check-seat.middleware.js';
-import { AddReview } from '../controllers/review.controller.js';
+import express from "express"
+import {
+  createOrder,
+  verifyPayment,
+  viewBookingbyUser,
+} from "../controllers/booking.controller.js"
+import { MovieDetails, Movies } from "../controllers/movie.controller.js"
+import { AddReview } from "../controllers/review.controller.js"
+import { GetShowsByDate, ShowSeats } from "../controllers/show.controller.js"
+import  {
+  Logout,
+  resendVerification,
+  setup2FA,
+  Signin,
+  Signup,
+  verify2FA,
+  verifyEmail,
+  checkUser,
+  getUser,
+} from "../controllers/user.controller.js"
+import checkSeatStatus from "../middlewares/check-seat.middleware.js"
+import authenticateUser from "../middlewares/user.middleware.js"
+import rateLimit from "express-rate-limit"
+import validateUserInput from "../middlewares/validaterequest.middleware.js"
+import { loginSchema, userSchema } from "../validator/uservalidator.js"
 
+const router = express.Router()
 
-const router = express.Router();
+router.post("/signup",validateUserInput(userSchema) ,Signup)
+router.post("/signin",rateLimit(15, 15 * 60 * 1000),validateUserInput(loginSchema), Signin)
+router.post("/logout", Logout)
+router.get("/check-user", checkUser)
+router.get("/movies", Movies)
+router.get("/movie-details/:id", authenticateUser, MovieDetails)
+router.get("/shows", authenticateUser, GetShowsByDate)
+router.get("/show-seats/:showId", authenticateUser, ShowSeats)
 
-router.post('/signup', Signup);
-router.post('/signin', Signin);
-router.post('/logout',Logout);
-router.get("/check-user",authenticateUser,checkUser)
-router.get('/movies',Movies)
-router.get('/movie-details/:id',authenticateUser,MovieDetails);
-router.get('/shows',authenticateUser,GetShowsByDate);
-router.get('/show-seats/:showId',authenticateUser,ShowSeats )
-
-router.post('/create-order',authenticateUser,checkSeatStatus,createOrder);
-router.post('/verify-payment',authenticateUser,verifyPayment)
-router.get('/view-booking',authenticateUser,viewBookingbyUser)
-router.post('/add-review',authenticateUser,AddReview)
-router.get('/get-user',authenticateUser,getUser)
-
-
+router.post("/create-order", authenticateUser, checkSeatStatus, createOrder)
+router.post("/verify-payment", authenticateUser, verifyPayment)
+router.get("/view-booking", authenticateUser, viewBookingbyUser)
+router.post("/add-review", authenticateUser, AddReview)
+router.get("/get-user", authenticateUser, getUser)
+router.post('/setup-2fa', authenticateUser, setup2FA);
+router.post('/verify-2fa', authenticateUser, verify2FA);
+router.get('/verify-email', verifyEmail);
+router.post('/resend-verification', resendVerification);
 
 //additional stat routes
 
-export default router;
+export default router
