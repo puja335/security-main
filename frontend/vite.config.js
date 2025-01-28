3ff32a34a0722dae71a392338d17003fddcdb0e9
puja335 <shahipuja335@gmail.com>
2025-01-28 10:22:55 +0545

    config for https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import fs from "fs"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    https: {
+      key: fs.readFileSync("./server.key"), // Path to your key file
+      cert: fs.readFileSync("./server.crt"), // Path to your certificate file
+    },
+    port: 3000, // Optional: Specify the port for HTTPS
+  },
 })
