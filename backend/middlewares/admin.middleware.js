0e69c87a98f5fb93e2f8e6d2b431f309ee10c44c
puja335 <shahipuja335@gmail.com>
2025-01-28 23:15:53 +0545

    changes in admin.middleware

--- backend/middlewares/admin.middleware.js
+++ backend/middlewares/admin.middleware.js
@@ -1,18 +1,20 @@
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
 function authenticateAdmin(req, res, next) {
+  
   const token = req.cookies.token;
+  if(!token) return res.status(403).json({ error: "Unauthorized" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, owner) => {
-    // console.log(err);
+    console.log(err);
 
     if (err) return res.sendStatus(403);
 
     req.owner = owner;
-    
+
     if (req.owner.role !== "admin") {
       return res.send("not authenticated");
     }
@@ -20,4 +22,4 @@ function authenticateAdmin(req, res, next) {
   });
 }
 
-export default authenticateAdmin;;
+export default authenticateAdmin;
